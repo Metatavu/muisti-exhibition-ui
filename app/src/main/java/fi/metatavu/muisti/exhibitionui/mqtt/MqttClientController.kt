7de49f9dd4b39878c4e9524b36bdacbd1601dd97
Antti Leppä <antti.leppa@metatavu.fi>
2020-04-16 18:23:54 +0300

    Added support group device events

---
+++ app/src/main/java/fi/metatavu/muisti/exhibitionui/mqtt/MqttClientController.kt
@@ -0,0 +1,75 @@
+package fi.metatavu.muisti.exhibitionui.mqtt
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import fi.metatavu.muisti.api.client.infrastructure.UUIDAdapter
+import fi.metatavu.muisti.exhibitionui.BuildConfig
+
+/**
+ * Controller for MQTT operations
+ *
+ * @author Antti Leppä
+ * @author Antti Leinonen
+ */
+class MqttClientController {
+
+    companion object {
+
+        private val client = MuistiMqttClient("tcp://${BuildConfig.MQTT_BASE_URL}")
+        private val listeners = mutableListOf<MqttTopicListener<*>>()
+
+        private val trigger : (topic: String?, message : String) -> Unit = { topic, message ->
+            listeners.forEach {
+                if (it.topic == topic)
+                    it.handleMessage(message)
+            }
+        }
+
+        init {
+            client.connect()
+            client.setCallBack(MuistiMqttCallBack(trigger))
+        }
+
+        /**
+         * Publishes given payload as serialied JSON into given topic
+         *
+         * @param topic topic
+         * @param payload payload
+         */
+        fun publish(topic: String, payload: Any) {
+            val moshi = Moshi.Builder().add(UUIDAdapter()).add(KotlinJsonAdapterFactory()).build()
+            val adapter = moshi.adapter(payload.javaClass)
+            val message = adapter.toJson(payload)
+            client.publish(topic, message)
+        }
+
+        /**
+         * Adds list of new MQTT topic listeners
+         *
+         * @param newListeners new listeners
+         */
+        fun addListeners(newListeners: List<MqttTopicListener<*>>) {
+            listeners.addAll(newListeners)
+        }
+
+        /**
+         * Adds new MQTT topic listener
+         *
+         * @param newListener new listener
+         */
+        fun addListener(newListener: MqttTopicListener<*>) {
+            listeners.add(newListener)
+        }
+
+        /**
+         * Removes a MQTT topic listener
+         *
+         * @param removeListener listener to be removed
+         */
+        fun  removeListener(removeListener: MqttTopicListener<*>) {
+            listeners.remove(removeListener)
+        }
+
+    }
+
+}
--- app/src/main/java/fi/metatavu/muisti/exhibitionui/services/UpdatePagesService.kt
+++ app/src/main/java/fi/metatavu/muisti/exhibitionui/services/UpdatePagesService.kt
@@ -68,7 +68,11 @@ object UpdatePages : MqttActionInterface {
             val exhibitionId = DeviceSettings.getExhibitionId()
             if (exhibitionId != null) {
                 val pages =
-                    MuistiApiFactory.getExhibitionPagesApi().listExhibitionPages(exhibitionId)
+                    MuistiApiFactory.getExhibitionPagesApi().listExhibitionPages(
+                        exhibitionId = exhibitionId,
+                        exhibitionContentVersionId = null,
+                        exhibitionDeviceId = null
+                    )
                 addPages(pages)
             }
         }
