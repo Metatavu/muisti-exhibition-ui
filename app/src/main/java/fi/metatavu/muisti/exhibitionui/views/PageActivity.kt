dafdcd3e873c13c88b5c6d027a12b135f31e88e3
Antti Leinonen <antti.pekka@gmail.com>
2020-04-17 11:25:09 +0300

    Update app/src/main/java/fi/metatavu/muisti/exhibitionui/views/PageActivity.kt
    Co-Authored-By: Antti Leppä <antti.leppa@metatavu.fi>

--- app/src/main/java/fi/metatavu/muisti/exhibitionui/views/PageActivity.kt
+++ app/src/main/java/fi/metatavu/muisti/exhibitionui/views/PageActivity.kt
@@ -332,7 +332,7 @@ class PageActivity : MuistiActivity() {
     private fun indexButtonClick() {
         clickCounterHandler.removeCallbacksAndMessages("index")
         settingsClickCOunter += 1
-        if(settingsClickCOunter > 4){
+        if (settingsClickCOunter > 4) {
             startMainActivity()
         } else {
             clickCounterHandler.postDelayed( {
