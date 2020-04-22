c780a46b3bc7b0c9f6150f3b6caa7afdbe86b207
platonovsimeon <simeon.platonov@gmail.com>
2020-04-22 14:11:08 +0300

    added test framework to Docker

--- app/src/test/java/fi/metatavu/muisti/exhibitionui/AppiumTest.kt
+++ app/src/test/java/fi/metatavu/muisti/exhibitionui/AppiumTest.kt
@@ -8,6 +8,8 @@ import org.junit.Test
 import org.openqa.selenium.WebElement
 import org.openqa.selenium.remote.DesiredCapabilities
 import java.io.File
+import java.lang.RuntimeException
+import java.net.ConnectException
 import java.net.URL
 
 class AppiumTest {
@@ -15,8 +17,7 @@ class AppiumTest {
     @Test
     @Throws(Exception::class)
     fun testApp() {
-        val classpathRoot = File(System.getProperty("user.dir")!!)
-        val app = File(classpathRoot.absolutePath, "app-debug.apk")
+        val app = File("/root/tmp", "app-debug.apk")
         val capabilities = DesiredCapabilities()
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android")
         capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0")
@@ -26,17 +27,19 @@ class AppiumTest {
         capabilities.setCapability(MobileCapabilityType.APP_PACKAGE, "fi.metatavu.muisti.exhibitionui")
         capabilities.setCapability(MobileCapabilityType.APP_ACTIVITY, "views.MainActivity")
         val driver: AndroidDriver<WebElement> = AndroidDriver(URL("http://localhost:4723/wd/hub"), capabilities)
-        Thread.sleep(5000)
-        val dropdowns = driver.findElementsByXPath("//*").filter { it.getAttribute("class") == "android.widget.LinearLayout" && it.getAttribute("clickable") == "true"}
+        Thread.sleep(10000)
+        val dropdowns = driver.findElementsByXPath("//*")
+            .filter { it.getAttribute("class") == "android.widget.LinearLayout" && it.getAttribute("clickable") == "true" }
         dropdowns[0].click()
-        Thread.sleep(500)
-        driver.findElementsByXPath("//*").filter { it.getAttribute("class") == "android.widget.ListView"}[0].click()
-        Thread.sleep(500)
+        Thread.sleep(5000)
+        driver.findElementsByXPath("//*").filter { it.getAttribute("class") == "android.widget.ListView" }[0].click()
+        Thread.sleep(5000)
         dropdowns[1].click()
-        Thread.sleep(500)
-        driver.findElementsByXPath("//*").filter { it.getAttribute("class") == "android.widget.CheckedTextView"}[1].click()
+        Thread.sleep(5000)
+        driver.findElementsByXPath("//*").filter { it.getAttribute("class") == "android.widget.CheckedTextView" }[1].click()
         driver.pressKeyCode(AndroidKeyCode.BACK)
-        Thread.sleep(1000)
-        Assert.assertEquals("PREVIEW PAGE", driver.findElementsByXPath("//*").filter { it.getAttribute("class") == "android.widget.Button"}[0].text);
+        Thread.sleep(10000)
+        Assert.assertEquals("PREVIEW PAGE", driver.findElementsByXPath("//*").filter { it.getAttribute("class") == "android.widget.Button" }[0].text)
+        driver.quit()
     }
 }
