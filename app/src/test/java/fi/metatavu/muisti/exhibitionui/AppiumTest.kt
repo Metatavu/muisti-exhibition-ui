5b8ec038a907023a6d2790bb1262a547b868b472
Antti Leppä <antti.leppa@metatavu.fi>
2020-04-22 10:19:29 +0300

    Added support for running api based tests

--- app/src/test/java/fi/metatavu/muisti/exhibitionui/AppiumTest.kt
+++ app/src/test/java/fi/metatavu/muisti/exhibitionui/AppiumTest.kt
@@ -15,28 +15,6 @@ class AppiumTest {
     @Test
     @Throws(Exception::class)
     fun testApp() {
-        val classpathRoot = File(System.getProperty("user.dir")!!)
-        val app = File(classpathRoot.absolutePath, "app-debug.apk")
-        val capabilities = DesiredCapabilities()
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android")
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0")
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator")
-        capabilities.setCapability("app", app.absolutePath)
-        capabilities.setCapability("automationName", "UiAutomator2")
-        capabilities.setCapability(MobileCapabilityType.APP_PACKAGE, "fi.metatavu.muisti.exhibitionui")
-        capabilities.setCapability(MobileCapabilityType.APP_ACTIVITY, "views.MainActivity")
-        val driver: AndroidDriver<WebElement> = AndroidDriver(URL("http://localhost:4723/wd/hub"), capabilities)
-        Thread.sleep(5000)
-        val dropdowns = driver.findElementsByXPath("//*").filter { it.getAttribute("class") == "android.widget.LinearLayout" && it.getAttribute("clickable") == "true"}
-        dropdowns[0].click()
-        Thread.sleep(500)
-        driver.findElementsByXPath("//*").filter { it.getAttribute("class") == "android.widget.ListView"}[0].click()
-        Thread.sleep(500)
-        dropdowns[1].click()
-        Thread.sleep(500)
-        driver.findElementsByXPath("//*").filter { it.getAttribute("class") == "android.widget.CheckedTextView"}[1].click()
-        driver.pressKeyCode(AndroidKeyCode.BACK)
-        Thread.sleep(1000)
-        Assert.assertEquals("PREVIEW PAGE", driver.findElementsByXPath("//*").filter { it.getAttribute("class") == "android.widget.Button"}[0].text);
+
     }
 }
