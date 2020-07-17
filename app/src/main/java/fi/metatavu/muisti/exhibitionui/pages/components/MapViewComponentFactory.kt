fc87abdd182cbc7b9de2964d351453d2b5c839f2
Simeon <simeon.platonov@gmail.com>
2020-07-17 08:48:57 +0300

    Started working on visualization

--- app/src/main/java/fi/metatavu/muisti/exhibitionui/pages/components/MapViewComponentFactory.kt
+++ app/src/main/java/fi/metatavu/muisti/exhibitionui/pages/components/MapViewComponentFactory.kt
@@ -1,9 +1,15 @@
 package fi.metatavu.muisti.exhibitionui.pages.components
 import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource
 import fi.metatavu.muisti.exhibitionui.pages.PageViewLifecycleListener
 import fi.metatavu.muisti.exhibitionui.views.PageActivity
+import mil.nga.geopackage.GeoPackage
 import mil.nga.geopackage.GeoPackageManager
+import mil.nga.geopackage.db.GeoPackageConnection
+import org.json.JSONArray
+import org.json.JSONObject
 import java.io.File
+import java.sql.ResultSet
 
 
 /**
@@ -40,8 +46,34 @@ private class MapViewLifeCycleListener(val view: MapView, val offLineFile: File,
     override fun onPageActivate(pageActivity: PageActivity) {
         val geoPackage = GeoPackageManager.open(offLineFile)
         val results = geoPackage.connection.query(sqlFilterQuery, ArrayList<String>().toTypedArray())
+        val geoJsonSource = MapViewUtils().constructGeoJson(results, geoPackage)
+
+
     }
 
     override fun onPageDeactivate(pageActivity: PageActivity) {
+    }
+}
+
+private class MapViewUtils() {
+    fun constructGeoJson(dbResults: ResultSet, geoPackage: GeoPackage): GeoJsonSource {
+        val featureCollection = JSONObject()
+        featureCollection.put("type", "FeatureCollection")
+        while(dbResults.next()) {
+            val deathPlaceId = dbResults.getInt("deathPlaceId")
+
+            if (deathPlaceId != null) {
+                val feature = JSONObject()
+                feature.put("type", "Feature")
+                val geometry = JSONObject()
+                val coordinates = JSONArray()
+
+                val point = geoPackage.getFeatureDao("locations").query("id == $deathPlaceId").geometry.geometry.centroid
+
+                //Finish geometry
+            }
+        }
+
+        return GeoJsonSource("deaths", featureCollection.toString())
     }
 }
