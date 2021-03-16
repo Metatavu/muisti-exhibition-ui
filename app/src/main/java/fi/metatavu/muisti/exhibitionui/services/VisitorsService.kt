8dc7d6c1c702e2e126bd852d0042b07b16476279
Antti-Metatavu <antti.leinonen@metatavu.fi>
2021-03-16 16:16:48 +0200

    Added try catching for visitor and session updates.

--- app/src/main/java/fi/metatavu/muisti/exhibitionui/services/VisitorsService.kt
+++ app/src/main/java/fi/metatavu/muisti/exhibitionui/services/VisitorsService.kt
@@ -1,12 +1,14 @@
 package fi.metatavu.muisti.exhibitionui.services
 
 import android.content.Intent
+import android.util.Log
 import androidx.core.app.JobIntentService
 import fi.metatavu.muisti.exhibitionui.api.MuistiApiFactory
 import fi.metatavu.muisti.exhibitionui.settings.DeviceSettings
 import fi.metatavu.muisti.exhibitionui.visitors.ExhibitionVisitorsContainer
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import java.lang.Exception
 
 /**
  * Service for caching Visitors and Visitor sessions.
@@ -22,17 +24,25 @@ class VisitorsService : JobIntentService() {
      * Updates the visitors list from the database
      */
     private fun updateVisitors() = GlobalScope.launch {
-        val exhibitionId = DeviceSettings.getExhibitionId() ?: return@launch
-        val visitorsList = MuistiApiFactory.getVisitorsApi().listVisitors(exhibitionId = exhibitionId, email = null, tagId = null)
-        ExhibitionVisitorsContainer.setVisitors(visitorsList)
+        try {
+            val exhibitionId = DeviceSettings.getExhibitionId() ?: return@launch
+            val visitorsList = MuistiApiFactory.getVisitorsApi().listVisitors(exhibitionId = exhibitionId, email = null, tagId = null)
+            ExhibitionVisitorsContainer.setVisitors(visitorsList)
+        } catch (e: Exception) {
+            Log.e(javaClass.name, "Error updating Exhibition visitor list: $e")
+        }
     }
 
     /**
      * Updates the visitor sessions list from the database
      */
     private fun updateVisitorSessions() = GlobalScope.launch {
-        val exhibitionId = DeviceSettings.getExhibitionId() ?: return@launch
-        val visitorSessionList = MuistiApiFactory.getVisitorSessionsApi().listVisitorSessions(exhibitionId = exhibitionId, tagId = null)
-        ExhibitionVisitorsContainer.setVisitorSessions(visitorSessionList)
+        try {
+            val exhibitionId = DeviceSettings.getExhibitionId() ?: return@launch
+            val visitorSessionList = MuistiApiFactory.getVisitorSessionsApi().listVisitorSessions(exhibitionId = exhibitionId, tagId = null)
+            ExhibitionVisitorsContainer.setVisitorSessions(visitorSessionList)
+        } catch (e: Exception) {
+            Log.e(javaClass.name, "Error updating Exhibition visitor session list: $e")
+        }
     }
 }
