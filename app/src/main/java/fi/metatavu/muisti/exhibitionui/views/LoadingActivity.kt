89e29e1f7a678f9186ed525e3762b1571e3c821f
Antti Leppä <antti.leppa@metatavu.fi>
2021-04-21 12:43:34 +0300

    Moved initial contents loading to separate LoadingActivity.kt

---
+++ app/src/main/java/fi/metatavu/muisti/exhibitionui/views/LoadingActivity.kt
@@ -0,0 +1,64 @@
+package fi.metatavu.muisti.exhibitionui.views
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import fi.metatavu.muisti.exhibitionui.R
+import fi.metatavu.muisti.exhibitionui.services.ConstructPagesService
+import fi.metatavu.muisti.exhibitionui.services.UpdateLayouts
+import fi.metatavu.muisti.exhibitionui.services.UpdatePages
+import kotlinx.android.synthetic.main.activity_page.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+/**
+ * Loading activity class
+ */
+class LoadingActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading)
+        loadContents()
+
+        settings_button.setOnClickListener {
+            startSettingsActivity()
+        }
+    }
+
+    /**
+     * Preloads all devices contents and moves to main activity
+     */
+    private fun loadContents() = GlobalScope.launch {
+        Log.d(LoadingActivity::class.java.name, "Loading layouts...")
+        UpdateLayouts.updateAllLayouts().join()
+
+        Log.d(LoadingActivity::class.java.name, "Loading pages...")
+        UpdatePages.updateAllPages().join()
+
+        Log.d(LoadingActivity::class.java.name, "Constructing pages...")
+        ConstructPagesService.constructAllPages().join()
+
+        Log.d(LoadingActivity::class.java.name, "Load ready, starting main activity...")
+
+        startMainActivity()
+    }
+
+    /**
+     * Starts main activity
+     */
+    private fun startMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        this.startActivity(intent)
+    }
+
+    /**
+     * Starts settings activity
+     */
+    private fun startSettingsActivity() {
+        val intent = Intent(this, SettingsActivity::class.java)
+        this.startActivity(intent)
+    }
+
+}
