bb0417ab4e902695320c6812499c813fd9ade6a4
Antti Leppä <antti.leppa@metatavu.fi>
2021-04-30 15:39:49 +0300

    Fixed issue #159

--- app/src/main/java/fi/metatavu/muisti/exhibitionui/actions/NavigatePageActionProvider.kt
+++ app/src/main/java/fi/metatavu/muisti/exhibitionui/actions/NavigatePageActionProvider.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import fi.metatavu.muisti.api.client.models.ExhibitionPageEventActionType
 import fi.metatavu.muisti.api.client.models.ExhibitionPageEventProperty
 import fi.metatavu.muisti.exhibitionui.ExhibitionUIApplication
+import fi.metatavu.muisti.exhibitionui.pages.PageViewContainer
 import fi.metatavu.muisti.exhibitionui.views.MuistiActivity
 import fi.metatavu.muisti.exhibitionui.views.PageActivity
 
@@ -21,12 +22,19 @@ class NavigatePageActionProvider(properties: Array<ExhibitionPageEventProperty>)
     override fun performAction(activity: MuistiActivity) {
         val pageId = getPropertyUuid("pageId")
         if (pageId != null) {
+            if (PageViewContainer.getPageView(pageId) == null) {
+                Log.d(this.javaClass.name, "Tried to navigate to non-existing page $pageId")
+                return
+            }
+
             val application = activity.applicationContext as ExhibitionUIApplication
             val currentActivity = application.getCurrentActivity()
 
             if (currentActivity is PageActivity) {
                 currentActivity.goToPage(pageId, currentActivity.transitionElements)
             }
+        } else {
+            Log.d(this.javaClass.name, "Tried to navigate to null page")
         }
     }
 
