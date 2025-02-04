e598a091af53bf01fae4d3b2cd3dc768f883d56f
hmiko01 <mikko.kosonen@xamk.fi>
2025-02-04 10:50:50 +0200

    Added missing sign_store_file

--- sync-env.sh
+++ sync-env.sh
@@ -13,6 +13,7 @@ echo SIGN_KEY_ALIAS=$SIGN_KEY_ALIAS >> local.properties
 echo SIGN_KEY_PASSWORD=$SIGN_KEY_PASSWORD >> local.properties
 
 echo $SIGN_KEYSTORE > /tmp/upload-keystore.b64 && base64 -d -i /tmp/upload-keystore.b64 > /tmp/upload-keystore.jks
+echo SIGN_STORE_FILE=/tmp/upload-keystore.jks >> local.properties
 
 echo MUISTI_API_BASE_URL=$(echo $SECRETS|jq -r '.data.data.MUISTI_API_BASE_URL') > app/.env
 echo KEYCLOAK_URL=$(echo $SECRETS|jq -r '.data.data.KEYCLOAK_URL') >> app/.env
