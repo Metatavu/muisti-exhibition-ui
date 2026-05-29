eca87966ddd11d928ec8e9ef3f58632e05338ffc
Antti Leppä <antti.leppa@metatavu.fi>
2026-05-29 14:04:52 +0300

    Log current activity changes with previous and new page details

--- app/src/main/java/fi/metatavu/muisti/exhibitionui/ExhibitionUIApplication.kt
+++ app/src/main/java/fi/metatavu/muisti/exhibitionui/ExhibitionUIApplication.kt
@@ -105,8 +105,16 @@ class ExhibitionUIApplication : Application() {
      * @param activity activity
      */
     fun setCurrentActivity(activity: Activity?) {
+        val previousActivity = currentActivity
+        val previousPageId = if (previousActivity is PageActivity) previousActivity.pageId else null
+        val newPageId = if (activity is PageActivity) activity.pageId else null
+        val previousPageName = if (previousPageId != null) PageViewContainer.getPageView(previousPageId)?.page?.name else null
+        val newPageName = if (newPageId != null) PageViewContainer.getPageView(newPageId)?.page?.name else null
+
+        Log.d(javaClass.name, "Current activity changed from page $previousPageName ($previousPageId) to page $newPageName ($newPageId)")
         currentActivity = activity
     }
+    
 
     /**
      * Logs out the current visitor session
