9a5b8053f00db2d5fa33a4d3a1efcf1875451fa2
Antti Leppä <antti.leppa@metatavu.fi>
2026-05-29 15:02:58 +0300

    Refactor PlayerPageViewLifecycleListener to use video URI instead of MediaSource for improved clarity and maintainability

--- app/src/main/java/fi/metatavu/muisti/exhibitionui/pages/components/PlayerViewComponentFactory.kt
+++ app/src/main/java/fi/metatavu/muisti/exhibitionui/pages/components/PlayerViewComponentFactory.kt
@@ -12,7 +12,6 @@ import android.widget.FrameLayout
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
-import com.google.android.exoplayer2.source.MediaSource
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.ui.PlayerControlView
 import com.google.android.exoplayer2.ui.PlayerView
@@ -96,7 +95,6 @@ class PlayerViewComponentFactory : AbstractComponentFactory<PlayerComponentConta
         val showPreviousButton = getBooleanProperty(buildContext = buildContext, propertyName = "showPreviousButton") ?: false
         val showNextButton = getBooleanProperty(buildContext = buildContext, propertyName = "showNextButton") ?: false
 
-        val context = buildContext.context
         val parent = buildContext.parents.lastOrNull()
 
         val view = PlayerComponentContainer(
@@ -117,12 +115,8 @@ class PlayerViewComponentFactory : AbstractComponentFactory<PlayerComponentConta
 
         val offlineFile = getResourceOfflineFile(buildContext, "src")
         if (offlineFile != null) {
-            val dataSourceFactory: DataSource.Factory = DefaultDataSourceFactory(context, Util.getUserAgent(context, "ExhibitionUIApplication"))
-            val mediaItem = MediaItem.fromUri(Uri.fromFile(offlineFile))
-            val videoSource: MediaSource = ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(mediaItem)
-
             buildContext.addLifecycleListener(PlayerPageViewLifecycleListener(
-                videoSource = videoSource,
+                videoUri = Uri.fromFile(offlineFile),
                 view = view,
                 autoPlay = autoPlay,
                 autoPlayDelay = autoPlayDelay
@@ -162,7 +156,7 @@ class PlayerViewComponentFactory : AbstractComponentFactory<PlayerComponentConta
  * @property autoPlay whether player should start automatically
  */
 private class PlayerPageViewLifecycleListener(
-    val videoSource: MediaSource,
+    val videoUri: Uri,
     val view: PlayerComponentContainer,
     val autoPlay: Boolean,
     val autoPlayDelay: Long
@@ -180,6 +174,9 @@ private class PlayerPageViewLifecycleListener(
 
         val player = SimpleExoPlayer.Builder(context).build()
         this.player = player
+        val dataSourceFactory: DataSource.Factory = DefaultDataSourceFactory(context, Util.getUserAgent(context, "ExhibitionUIApplication"))
+        val mediaItem = MediaItem.fromUri(videoUri)
+        val videoSource = ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(mediaItem)
 
         if (autoPlay && autoPlayDelay > 0) {
             autoPlayRunnable = Runnable {
