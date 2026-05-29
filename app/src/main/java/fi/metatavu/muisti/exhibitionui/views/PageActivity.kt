5ba3f883f055fe443902c177bf49ece07a9085a9
Antti Leppä <antti.leppa@metatavu.fi>
2026-05-29 14:26:33 +0300

    Fix import statement for PageViewContainer in PageActivity

--- app/src/main/java/fi/metatavu/muisti/exhibitionui/views/PageActivity.kt
+++ app/src/main/java/fi/metatavu/muisti/exhibitionui/views/PageActivity.kt
@@ -16,6 +16,7 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import java.util.*
 import kotlin.math.roundToInt
+import fi.metatavu.muisti.exhibitionui.pages.PageViewContainer
 
 /**
  * Activity for displaying pages from API
